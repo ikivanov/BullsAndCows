65c7b6ded50b05d5f4f45a564ec8b2aaed1d6ef6
ikivanov <ikivanov@gmail.com>
2015-08-09 15:57:49 +0300

    refactoring: player mode abstraction introduced

--- Client/app.js
+++ Client/app.js
@@ -5,9 +5,17 @@
         SERVER_ADDRESS: "localhost:1337",
         MISSING_HOST_ELEMENT_MESSAGE: "Host element cannot be null, undentified or an empty string!",
         INVALID_HOST_ELEMENT_MESSAGE: "Invalid host element! A valid html element identifier or jquery html object should be provided!",
+        INVALID_PLAYER_MODE_MESSAGE: "Invalid player mode!"
     };
 
-    App.Run = function (hostElement) {
+    App.playerModes = {
+        HUMAN_VS_COMPUTER: 0,
+        COMPUTER_VS_COMPUTER: 1,
+        MULTIPLAYER: 2,
+        PEER_2_PEER: 3
+    }
+
+    App.Run = function (hostElement, mode) {
         if (!hostElement) {
             throw new Error(consts.MISSING_HOST_ELEMENT_MESSAGE);
         }
@@ -18,14 +26,34 @@
             throw new Error(consts.INVALID_HOST_ELEMENT_MESSAGE);
         }
 
-        render(el);
+        var playerMode = PlayerModeFactory.Create(mode);
+        if (!playerMode) throw new Error(consts.INVALID_PLAYER_MODE_MESSAGE);
+
+        playerMode.render(el);
     }
 
-    var render = function (hostElement) {
-        var that = this;
+    var PlayerMode = function (view, viewModel) {
+        this.view = view,
+        this.viewModel = viewModel,
 
-        hostElement.load("views\\computerVsComputerView.html", function () {
-            ko.applyBindings(new ComputerVsComputerViewModel());
-        });
+        this.render = function (hostElement) {
+            var that = this;
+            hostElement.load(that.view, function () {
+                ko.applyBindings(new that.viewModel());
+            });
+        }
+    }
+
+    var PlayerModeFactory = {}
+    PlayerModeFactory.modes = {};
+    PlayerModeFactory.modes[App.playerModes.HUMAN_VS_COMPUTER] = new PlayerMode("views\\humanVsComputerView.html", HumanVsComputerViewModel);
+    PlayerModeFactory.modes[App.playerModes.COMPUTER_VS_COMPUTER] = new PlayerMode("views\\computerVsComputerView.html", ComputerVsComputerViewModel);
+
+    PlayerModeFactory.Create = function (mode) {
+        if (!PlayerModeFactory.modes[mode]) {
+            return null;
+        }
+
+        return PlayerModeFactory.modes[mode];
     }
 })();
